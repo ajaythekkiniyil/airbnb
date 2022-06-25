527021ecea20bccc28145734b0b277a32833dd23
ajaythekkiniyi <ajaythekkiniyi@gmail.com>
2022-06-25 17:18:37 +0530

    updated head

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
       <div className="container mt-5">
         <div className="row">
           <div className="col-md-4 mt-md-5">
-              <h3 className='mt-md-5'>Online Experience</h3>
+              <h3 className='mt-md-5'>Online Experience Fell free</h3>
               <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly 
                 used to demonstrate the visual form of a document or a typeface without 
                 relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.
